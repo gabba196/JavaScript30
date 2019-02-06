2d2961a80e851d11a401a601451d5326cd7645e9
gabba196 <g_arusoae@outlook.com>
2019-02-06 11:15:22 +0000

    change

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -3,6 +3,7 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+
 function setDate () {
   const now = new Date();
 
